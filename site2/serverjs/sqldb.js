8324e9107b4bac5e72d675f686fa9eb676409101
Andrew <ginns.aw@gmail.com>
2018-05-20 14:11:28 +0100

    Added filler text and load button scroll

--- site2/serverjs/sqldb.js
+++ site2/serverjs/sqldb.js
@@ -46,9 +46,9 @@ function sqlDB() {
         insertBlogQuery = db.prepare("INSERT OR IGNORE INTO Blogs(title, message, date) VALUES(?,?,?)");
         selectBlogQuery = db.prepare("SELECT * FROM Blogs WHERE rowid = ?");
 
-        addBlog("title", "mesage", "2");
-        addBlog("title2", "mesage", "1");
-        addBlog("title3", "mesage", "4");
+        addBlog("The Rage of Achilles", "The poet invokes a muse to aid him in telling the story of the rage of Achilles, the greatest Greek hero to fight in the Trojan War. The narrative begins nine years after the start of the war, as the Achaeans sack a Trojan-allied town and capture two beautiful maidens, Chryseis and Briseis. Agamemnon, commander-in-chief of the Achaean army, takes Chryseis as his prize. Achilles, one of the Achaeans’ most valuable warriors, claims Briseis. Chryseis’s father, a man named Chryses who serves as a priest of the god Apollo, begs Agamemnon to return his daughter and offers to pay an enormous ransom. When Agamemnon refuses, Chryses prays to Apollo for help.", "1");
+        addBlog("The Great Gathering of Armies", "To help the Trojans, as promised, Zeus sends a false dream to Agamemnon in which a figure in the form of Nestor persuades Agamemnon that he can take Troy if he launches a full-scale assault on the city’s walls. The next day, Agamemnon gathers his troops for attack, but, to test their courage, he lies and tells them that he has decided to give up the war and return to Greece. To his dismay, they eagerly run to their ships.", "2");
+        addBlog("Helen Reviews the Champions", "The Trojan army marches from the city gates and advances to meet the Achaeans. Paris, the Trojan prince who precipitated the war by stealing the beautiful Helen from her husband, Menelaus, challenges the Achaeans to single combat with any of their warriors. When Menelaus steps forward, however, Paris loses heart and shrinks back into the Trojan ranks. Hector, Paris’s brother and the leader of the Trojan forces, chastises Paris for his cowardice. Stung by Hector’s insult, Paris finally agrees to a duel with Menelaus, declaring that the contest will establish peace between Trojans and Achaeans by deciding once and for all which man shall have Helen as his wife. Hector presents the terms to Menelaus, who accepts. Both armies look forward to ending the war at last.", "3");
         db.run("CREATE TABLE IF NOT EXISTS BlogsOrdered(title TEXT NOT NULL, message TEXT NOT NULL, date INTEGER NOT NULL)");
         db.run("INSERT INTO BlogsOrdered(title, message, date) SELECT title,message,date FROM Blogs ORDER BY date ASC");
         db.run("DROP TABLE Blogs");
@@ -63,9 +63,9 @@ function sqlDB() {
         insertProjectQuery = db.prepare("INSERT OR IGNORE INTO Projects(title, message, date) VALUES(?,?,?)");
         selectProjectQuery = db.prepare("SELECT * FROM Projects WHERE rowid = ?");
 
-        addProject("project1", "mesage", "2");
-        addProject("project2", "mesage", "1");
-        addProject("project 3", "mesage", "4");
+        addProject("Apollo", "NASA's best space science mission? The one humans got to tag along on, of course! Not only was sending a man to the moon monumental for human history, but the Apollo trips were the first to bring celestial stuff back to Earth and greatly advanced our scientific understanding of the moon. Before Apollo, many people weren't even convinced the moon wasn't made out of cheese (well? non-scientists at least). By studying the moon up close and personal, and then carting? loads of moon rocks home, the Apollo astronauts gathered data that helped us learn how old the moon is, what it's made out of, and even how it might have begun.", "1");
+        addProject("Hubble", "The most-loved of all NASA spacecraft, the Hubble Space Telescope has name recognition around the world. Its photos have changed the way everyday people figure themselves into the cosmos. The observatory has also radically changed science, making breakthroughs on astronomical issues too numerous to count. By finally sending up an optical telescope to peer at the sky from beyond Earth's turbulent atmosphere, NASA developed a tool that could reveal stars, planets, nebulae and galaxies in all their fully-detailed glory.", "2");
+        addProject("Viking", "When NASA's Viking 1 probe touched-down on Mars in July 1976, it was the first time a man-made object had soft-landed on the red planet. (Though the Soviet Mars 2 and 3 probes did land on the surface, they failed upon landing). The Viking 1 lander also holds the title of longest-running Mars surface mission, with a total duration of 6 years and 116 days. The spacecraft also sent the first color pictures back from the Martian surface, showing us what that mysterious red dot looks like from the ground for the first time.", "3");
         
         db.run("CREATE TABLE IF NOT EXISTS ProjectsOrdered(title TEXT NOT NULL, message TEXT NOT NULL, date INTEGER NOT NULL)");
         db.run("INSERT INTO ProjectsOrdered(title, message, date) SELECT title,message,date FROM Projects ORDER BY date ASC");
